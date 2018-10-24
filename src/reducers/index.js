8e4cf9246691bc4f2e8af2cb5ff7deb39c21c5a8
Derek <derekjohnbaker23@gmail.com>
2018-10-24 17:56:22 -0500

    1.0

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
+import reducer_active_book from './reducer_active_book';
 
 const rootReducer = combineReducers({
-    books: BooksReducer
+    books: BooksReducer,
+    activeBook: reducer_active_book
 });
 
 export default rootReducer;
